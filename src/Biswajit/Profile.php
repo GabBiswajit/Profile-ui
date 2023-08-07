ae113aa5ed1386acaefc5598a0f6ef60a994ce09
Biswajit <121815367+GabBiswajit@users.noreply.github.com>
2023-08-07 23:44:04 +0530

    Update Profile.php

--- src/Biswajit/Profile.php
+++ src/Biswajit/Profile.php
@@ -8,8 +8,7 @@ use pocketmine\plugin\PluginBase;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-
-use libs\jojoe77777\FormAPI\SimpleForm;
+use cooldogedev\BedrockEconomy\libs\cooldogedev\libSQL\context\ClosureContext;
 
 class Profile extends PluginBase {
 
@@ -47,9 +46,11 @@ class Profile extends PluginBase {
         $p = $sender;
         $name = $p->getName();
         $rank = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($p)->getName();
-        $eco = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        $eco = Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy")->getAPI();
+        $economy->getPlayerBalance($player->getName(),
+        ClosureContext::create(function (?int $balance) use($player): void {
+        $this->playerMoney = $balance }, ));
         $date = date("d/m/Y H:i:s");
-        $money = $eco->money($p);
         $ping = $p->getNetworkSession()->getPing();
         $world = $p->getWorld()->getProvider()->getWorldData()->getName();
         $x = $p->getPosition()->getX();
@@ -57,7 +58,7 @@ class Profile extends PluginBase {
         $z = $p->getPosition()->getZ();
         
         $form->setTitle("§l§cYOUR PROFILE");
-        $form->setContent("§r§9This Is Your Profile On This Server:\n\n§7".$date."\n§fName : §a".$name."\n§fRank : §a".$rank."\n§fMoney : §a".$money."\n§fPing : §a".$ping."\n§fPosition : §a".$x." ".$y." ".$z."\n§fFirst Join : §a".date("F, j Y H:i:s", (int)($sender->getFirstPlayed() / 1000))." WIB");
+        $form->setContent("§r§9This Is Your Profile On This Server:\n\n§7".$date."\n§fName : §a".$name."\n§fRank : §a".$rank."\n§fMoney : §a".$balance."\n§fPing : §a".$ping."\n§fPosition : §a".$x." ".$y." ".$z."\n§fFirst Join : §a".date("F, j Y H:i:s", (int)($sender->getFirstPlayed() / 1000))." WIB");
         $form->addButton("§l§cEXIT",0, "textures/blocks/barrier");
         $form->sendToPlayer($sender);
 
