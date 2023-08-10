005c505604266d33e457a5b9f9f72596529fe9c8
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-08-10 09:34:34 +0530

    Update Profile.php

--- src/Biswajit/ProfileUI/Profile.php
+++ src/Biswajit/ProfileUI/Profile.php
@@ -2,71 +2,109 @@
 
 namespace Biswajit\ProfileUI;
 
-use pocketmine\Server;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use jojoe77777\FormAPI\SimpleForm;
-use davidglitch04\libEco\libEco;
-use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as TF;
+use Biswajit\ProfileUI\libs\jojoe77777\FormAPI\SimpleForm;
+use Biswajit\ProfileUI\libs\davidglitch04\libEco\libEco;
 
-class Profile extends PluginBase implements Listener {
+class Profile extends PluginBase {
 
-    public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
-    switch($cmd->getName()){
-      case "profile":
-        if($sender instanceof Player){
-          $this->openMyForm($sender);
-        }else{
-        	
-          $sender->sendMessage("§cUse this command in game");
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "profile") {
+            if (!$sender instanceof Player) {
+                $sender->sendMessage(TF::RED . "Use this command in-game");
+                return false;
+            }
+            $this->openProfileForm($sender);
         }
-      break;
+        return true;
     }
-    return true;
-  } 
 
-    public function openMyForm($sender){
-        $form = new SimpleForm(function (Player $sender, $data){
-            $result = $data;
+    public function openProfileForm(Player $player): void {
+        $form = new SimpleForm(function (Player $player, ?int $data): void {
+            if ($data !== null) {
+                // NOOP
+            }
+        });
 
-            if($result === null){
-                return true;
+        libEco::myMoney($player,
+            function(float $money) use ($player, $form): void {
+                $name = $player->getName();
+                $rank = $this->getRank($player);
+                $expLevel = $player->getXpManager()->getXpLevel();
+                $date = date("d/m/Y H:i:s");
+                $ping = $this->getPing($player);
+                $firstPlayedDate = $this->getFormattedFirstPlayedDate($player);
+                $coords = $this->getCoords($player);
+
+                $form->setTitle(TF::BOLD . TF::RED . "YOUR PROFILE");
+                $content = "This is your profile on the server:\n\n" .
+                TF::GRAY . $date . "\n" .
+                TF::WHITE . "Name : " . TF::GREEN . $name . "\n" .
+                TF::WHITE . "Rank : " . TF::GREEN . $rank . "\n" .
+                TF::WHITE . "Money : " . TF::GREEN . $money . "\n" .
+                TF::WHITE . "Ping : " . TF::GREEN . $ping . "\n" .
+                TF::WHITE . "Experience : " . TF::GREEN . $expLevel . "\n" .
+                TF::WHITE . $coords . "\n" .
+                TF::WHITE . "First Join : " . TF::GREEN . $firstPlayedDate;
+
+                $form->setContent($content);
+                $form->addButton(TF::BOLD . TF::RED . "EXIT", 0, "textures/blocks/barrier");
+                $form->sendToPlayer($player);
+            });
+    }
+
+    private function getRank(Player $player): ?string {
+        $purePerms = $this->getPlugin("PurePerms");
+        $ranksystem = $this->getPlugin("RankSystem");
+
+        if ($purePerms !== null) {
+            $group = $purePerms->getUserDataMgr()->getGroup($player);
+            return $group !== null ? $group->getName() : null;
+        } elseif ($ranksystem !== null) {
+            $session = $ranksystem->getSessionManager()->get($player);
+            $rankNames = [];
+
+            foreach ($session->getRanks() as $rank) {
+                $rankNames[] = $rank->getName();
             }
 
-            switch($result){
-                case 0:              
+            return implode(', ', $rankNames);
+        }
 
-                break;
-
-             }
-
-        });
-        $player = $sender;
-        $name = $player->getName();
-        $rank = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName();
-        $eco = libEco::myMoney($player, static function(float $money) : void {
-	    var_dump($money);
-         });
-        $date = date("d/m/Y H:i:s");
-        $expLevel = $player->getXpLevel();
-        $ping = $player->getNetworkSession()->getPing();
-        $world = $player->getWorld()->getProvider()->getWorldData()->getName();
-        $x = $player->getPosition()->getX();
-        $y = $player->getPosition()->getY();
-        $z = $player->getPosition()->getZ();
-        
-        $form->setTitle("§l§cYOUR PROFILE");
-        $form->setContent("§r§9This Is Your Profile On This Server:\n\n§7".$date."\n§fName : §a".$name."\n§fRank : §a".$rank."\n§fMoney : §a".$balance."\n§fPing : §a".$ping."\n§fExperience : §a".$expLevel."\n§fPosition : §a".$x." ".$y." ".$z."\n§fFirst Join : §a".date("F, j Y H:i:s", (int)($sender->getFirstPlayed() / 1000))." WIB");
-        $form->addButton("§l§cEXIT",0, "textures/blocks/barrier");
-        $form->sendToPlayer($sender);
-
-        return $form;
-
+        return null;
     }
 
+    private function getPlugin(string $name) {
+        return $this->getServer()->getPluginManager()->getPlugin($name);
+    }
+
+    private function getFormattedFirstPlayedDate(Player $player): string {
+        $timestamp = (int)($player->getFirstPlayed() / 1000);
+        $formattedDate = date("F j, Y H:i:s", $timestamp);
+        return $formattedDate . " WIB";
+    }
+
+    private function getCoords(Player $player): string {
+        $x = (int) $player->getPosition()->getX();
+        $y = (int) $player->getPosition()->getY();
+        $z = (int) $player->getPosition()->getZ();
+
+        $position = TF::WHITE . "Position : " . TF::GREEN . $x . " " . $y . " " . $z . TF::RESET;
+
+        return $position;
+    }
+
+    private function getPing(Player $player): ?int {
+        $ping = $player->getNetworkSession()->getPing();
+        return $ping;
+    }
+
+    private function getWorldName(Player $player) {
+        $worldName = $player->getWorld()->getProvider()->getWorldData()->getName();
+        return $worldName;
+    }
 }
